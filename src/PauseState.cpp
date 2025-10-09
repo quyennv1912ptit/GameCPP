854183b8c1f4452e3c30f87394083cb9afa10823
quyennv1912 <quyennv19122006@gmail.com>
2025-10-09 21:19:03 +0700

    change buttons.

--- src/PauseState.cpp
+++ src/PauseState.cpp
@@ -19,17 +19,17 @@ void PauseState::Enter()
     resumeBtn->setPos({1280.0f / 2 - resumeDims.size.x / 2, 200});
     buttons.push_back(resumeBtn);
 
-    // quit
-    quitBtn = new CombinedButton(renderer, "resources/imgs/ui/quitBtn.png", "resources/imgs/ui/quitBtn_hovered.png", 40.0f, 40.0f, "resources/fonts/mightysouly.ttf", "Quit", 30.0f, {255, 255, 255, 255}, {100, 100, 100, 255});
-    Transform quitDims = quitBtn->getDims();
-    quitBtn->setPos({1280.0f / 2 - quitDims.size.x / 2, 275});
-    buttons.push_back(quitBtn);
-
     // restart
     restartBtn = new CombinedButton(renderer, "resources/imgs/ui/restartBtn.png", "resources/imgs/ui/restartBtn_hovered.png", 40.0f, 40.0f, "resources/fonts/mightysouly.ttf", "Restart", 30.0f, {255, 255, 255, 255}, {100, 100, 100, 255});
     Transform restartDims = restartBtn->getDims();
-    restartBtn->setPos({1280.0f / 2 - restartDims.size.x / 2, 350});
+    restartBtn->setPos({1280.0f / 2 - restartDims.size.x / 2, 275});
     buttons.push_back(restartBtn);
+
+    // quit
+    quitBtn = new CombinedButton(renderer, "resources/imgs/ui/quitBtn.png", "resources/imgs/ui/quitBtn_hovered.png", 40.0f, 40.0f, "resources/fonts/mightysouly.ttf", "Quit", 30.0f, {255, 255, 255, 255}, {100, 100, 100, 255});
+    Transform quitDims = quitBtn->getDims();
+    quitBtn->setPos({1280.0f / 2 - quitDims.size.x / 2, 350});
+    buttons.push_back(quitBtn);
 }
 
 void PauseState::HandleEvent(const SDL_Event &event)
