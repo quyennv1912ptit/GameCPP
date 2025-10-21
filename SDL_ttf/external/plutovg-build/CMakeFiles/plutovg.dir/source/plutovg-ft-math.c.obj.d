a4ca3b8b1dda083f15e7ed78e8349a12a1306aaa
quyennv1912ptit <quyennv19122006@gmail.com>
2025-10-21 08:30:29 +0700

    fix.

--- SDL_ttf/external/plutovg-build/CMakeFiles/plutovg.dir/source/plutovg-ft-math.c.obj.d
+++
@@ -1,4 +0,0 @@
-vendored/SDL_ttf/external/plutovg-build/CMakeFiles/plutovg.dir/source/plutovg-ft-math.c.obj: \
- C:\Code\CPP\vendored\SDL_ttf\external\plutovg\source\plutovg-ft-math.c \
- C:\Code\CPP\vendored\SDL_ttf\external\plutovg\source\plutovg-ft-math.h \
- C:\Code\CPP\vendored\SDL_ttf\external\plutovg\source\plutovg-ft-types.h
